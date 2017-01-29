472d56b85f03f07361cac32804e3b3e9cc1cf99b
Abhishek Srivastava <abhsrivastava@researchnow.com>
2017-01-28 18:57:06 -0600

    initial implementation

---
+++ build.sbt
@@ -0,0 +1,15 @@
+name := "Http4sTest2"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+
+val http4sVersion = "0.15.3"
+
+libraryDependencies ++= Seq(
+  "org.http4s" %% "http4s-core" % http4sVersion,
+  "org.http4s" %% "http4s-dsl" % http4sVersion,
+  "org.http4s" %% "http4s-blaze-client" % http4sVersion,
+  "org.http4s" %% "http4s-circe" % http4sVersion,
+  "io.circe" %% "circe-generic" % "0.1.0"
+)
---
+++ src/main/scala/com/abhi/Http4sTest.scala
@@ -0,0 +1,45 @@
+package com.abhi
+
+/**
+  * Created by ASrivastava on 1/25/17.
+  */
+
+import org.http4s.client.blaze.SimpleHttp1Client
+import org.http4s.Status.ResponseClass.Successful
+import io.circe.syntax._
+import org.http4s._
+import org.http4s.headers._
+import org.http4s.circe._
+import scalaz.concurrent.Task
+import io.circe._
+
+final case class Login(username: String, password: String)
+final case class Token(token: String)
+
+object JsonHelpers {
+   import io.circe.generic.auto._
+   implicit val tokenDecoder : Decoder[Token] = Decoder.forProduct1("token")(Token.apply)
+   implicit val tokenEncoder : Encoder[Token] = Encoder.forProduct1("token")(u => u.token)
+   implicit val loginDecoder : Decoder[Login] = Decoder.forProduct2("username", "password")(Login.apply)
+   implicit val loginEncoder : Encoder[Login] = Encoder.forProduct2("username", "password")(u => (u.username, u.password))
+   implicit val loginEntityEncoder : EntityEncoder[Login] = jsonEncoderOf[Login]
+   implicit val loginEntityDecoder : EntityDecoder[Login] = jsonOf[Login]
+   implicit val tokenEntityEncoder: EntityEncoder[Token] = jsonEncoderOf[Token]
+   implicit val tokenEntityDecoder : EntityDecoder[Token] = jsonOf[Token]
+}
+
+object Http4sTest2 extends App {
+   import JsonHelpers._
+   val url = "http://"
+   val uri = Uri.fromString(url).valueOr(throw _)
+   val list = List[Header](`Content-Type`(MediaType.`application/json`), `Accept`(MediaType.`application/json`))
+   val request = Request(uri = uri, method = Method.POST)
+      .withBody(Login("foo", "bar").asJson)
+      .map{r => r.replaceAllHeaders(list :_*)}.run
+   val client = SimpleHttp1Client()
+   val result = client.fetch[Option[Token]](request){
+      case Successful(response) => response.as[Token].map(Some(_))
+      case _ => Task(Option.empty[Token])
+   }.run
+   println(result)
+}
